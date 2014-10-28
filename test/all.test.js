ae027456210ffcd9a0ef65c656badaf001a8b5e5
overlookmotel <theoverlookmotel@gmail.com>
2014-10-28 12:41:27 +0000

    Tests

--- test/all.test.js
+++ test/all.test.js
@@ -15,4 +15,61 @@ chai.config.includeStack = true;
 // tests
 
 describe('toposort', function() {
+	it('sorts strings', function() {
+		var sorted = toposort([
+			['Ingredient', 'Shop'],
+			['Food', 'Ingredient']
+		]);
+		
+		expect(sorted).to.deep.equal(['Food', 'Ingredient', 'Shop']);
+	});
+	
+	it('sorts objects', function() {
+		var sorted = toposort([
+			[ { table: 'Ingredient' }, { table: 'Shop' } ],
+			[ { table: 'Food' }, { table: 'Ingredient' } ]
+		]);
+		
+		expect(sorted).to.deep.equal([ { table: 'Food' }, { table: 'Ingredient' }, { table: 'Shop' } ]);
+	});
+	
+	it('throws error on cyclic dependency', function() {
+		expect(function() {
+			toposort([
+				[ { table: 'Ingredient' }, { table: 'Shop' } ],
+				[ { table: 'Food' }, { table: 'Ingredient' } ],
+				[ { table: 'Shop' }, { table: 'Food' } ]
+			]);
+		}).to.throw(toposort.Error, 'Cyclic dependency: {"table":"Food"}');
+	});
+});
+
+describe('toposort.dependents', function() {
+	it('sorts strings', function() {
+		var sorted = toposort.dependents([
+			['Ingredient', 'Shop'],
+			['Food', 'Ingredient']
+		]);
+		
+		expect(sorted).to.deep.equal(['Food', 'Ingredient']);
+	});
+	
+	it('sorts objects', function() {
+		var sorted = toposort.dependents([
+			[ { table: 'Ingredient' }, { table: 'Shop' } ],
+			[ { table: 'Food' }, { table: 'Ingredient' } ]
+		]);
+		
+		expect(sorted).to.deep.equal([ { table: 'Food' }, { table: 'Ingredient' } ]);
+	});
+	
+	it('throws error on cyclic dependency', function() {
+		expect(function() {
+			toposort.dependents([
+				[ { table: 'Ingredient' }, { table: 'Shop' } ],
+				[ { table: 'Food' }, { table: 'Ingredient' } ],
+				[ { table: 'Shop' }, { table: 'Food' } ]
+			]);
+		}).to.throw(toposort.Error, 'Cyclic dependency: {"table":"Food"}');
+	});
 });
